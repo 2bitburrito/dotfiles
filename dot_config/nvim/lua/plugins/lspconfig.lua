442dda9ba4b50baf80cf753f0e4e1a30ef5f732a
Hugh Palmer <palmerhap@gmail.com>
2025-07-05 20:36:47 +1000

    saving before making big nvim change

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -1,41 +1,55 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "mason-org/mason-lspconfig.nvim",
-    -- "jose-elias-alvarez/typescript.nvim",
-    -- init = function()
-    --   require("lazyvim.util").lsp.on_attach(function(_, buffer)
-    --     -- stylua: ignore
-    --     vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
-    --     vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
-    --   end)
-    -- end,
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason-org/mason-lspconfig.nvim",
+      version = "1.32.0",
+      -- "jose-elias-alvarez/typescript.nvim",
+      -- init = function()
+      --   require("lazyvim.util").lsp.on_attach(function(_, buffer)
+      --   -- stylua: ignore
+      --   vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+      --     vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+      --   end)
+      -- end,
+    },
+    opts = {
+      inlay_hints = {
+        enabled = false,
+      },
+      servers = {
+        tsserver = false,
+        html = {},
+        gopls = {},
+      },
+      setup = {
+        tsserver = function(_, opts)
+          require("typescript").setup({ server = opts })
+          return true
+        end,
+      },
+      settings = {
+        autoformat = true,
+        format_on_save = true,
+        format_on_insert_leave = false,
+        format_on_insert_enter = false,
+        format_on_file_open = false,
+        experimental_flat_config = true,
+      },
+    },
   },
-  opts = {
-    inlay_hints = {
-      enabled = false,
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {
+      settings = {
+        tsserver_plugins = {},
+        complete_function_calls = true,
+        include_completions_with_insert_text = true,
+      },
     },
-    servers = {
-      tsserver = {},
-    },
-    setup = {
-      tsserver = function(_, opts)
-        require("typescript").setup({ server = opts })
-        return true
-      end,
-    },
-    settings = {
-      autoformat = true,
-      format_on_save = true,
-      format_on_insert_leave = false,
-      format_on_insert_enter = false,
-      format_on_file_open = false,
-      experimental_flat_config = true,
-    },
+    config = function(_, opts)
+      require("typescript-tools").setup(opts)
+    end,
   },
-  -- {
-  --   "pmizio/typescript-tools.nvim",
-  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  --   opts = {},
-  -- },
 }
